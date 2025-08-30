5d55742e27058b8b00a09e9d1a17c9feb8074a38
Nono Yuu <s0@ts-uc.org>
2025-08-30 10:12:12 +0900

    vercelでのデプロイに対応

---
+++ middleware.js
@@ -0,0 +1,34 @@
+const next = () => {
+  const headers = new Headers();
+  headers.set("x-middleware-next", "1");
+  return new Response(null, {
+    headers,
+  });
+};
+
+export const config = {
+  matcher: "/",
+};
+
+export default function middleware(request) {
+  const authorizationHeader = request.headers.get("authorization");
+
+  if (authorizationHeader) {
+    const basicAuth = authorizationHeader.split(" ")[1];
+    const [user, password] = atob(basicAuth).toString().split(":");
+
+    if (
+      user === process.env.BASIC_AUTH_USER &&
+      password === process.env.BASIC_AUTH_PASSWORD
+    ) {
+      return next();
+    }
+  }
+
+  return new Response("Basic Auth required", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Secure Area"',
+    },
+  });
+}
